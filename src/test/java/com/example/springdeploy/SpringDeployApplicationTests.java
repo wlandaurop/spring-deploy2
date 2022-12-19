b2bf04a7fffc4689746cf4b08289b4ded08fe322
wlandaurop <wlandaurop@gmail.com>
2022-12-19 20:38:21 +0100

    spring heroku app config

--- src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -3,13 +3,13 @@ package com.example.springdeploy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+// @SpringBootTest
 class SpringDeployApplicationTests {
 
 	@Test
 	void contextLoads() {
 		System.getenv().forEach(
-				(key, value) -> System.out.println(key + "" + value)
+				(key, value) -> System.out.println(key + " " + value)
 		);
 	}
 
