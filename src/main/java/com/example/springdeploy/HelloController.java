1d005fc5fd8c579fa0dc892aecebe7e60eeb5793
wlandaurop <wlandaurop@gmail.com>
2023-01-14 12:59:32 +0100

    ajustar url a pagina principal

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
         }
 
 
-        @GetMapping("/bootstrap")
+        @GetMapping("/")
         public String bootstrap(){
             return """
                 <!doctype html>
